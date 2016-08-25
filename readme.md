afc9017937afcbe72bd26c0d05b4da6210561f1f
South China University <340447244@qq.com>
2016-08-25 11:54:00 +0800

    update
    new update.

--- readme.md
+++ readme.md
@@ -7,3 +7,12 @@ author: shikanon <shikanon@foxmail.com>
 一个scrapy项目生成工具，适合新手使用，可以生成一个scrapy基本模板，减少配置烦恼。
 搭载了随机User-Agent，代理ip，redis等，
 写入文件采用jsonline格式。
+
+使用方法:
+`python genspider.py`
+`the new projects path:`在此输入生成的项目目录路径
+`the projects name:`在此输入项目名称
+转到项目目录下，执行
+`scrapy list`
+可以看到已生成的项目
+运行`scrapy crawl 项目名称` 开始抓取任务。
--- {{.project_name}}/{{.project_name}}/spiders/{{.project_name}}_spider.py
+++ {{.project_name}}/{{.project_name}}/spiders/{{.project_name}}_spider.py
@@ -7,12 +7,19 @@ class {{.project_name}}Spider(scrapy.Spider):
     name = "{{.project_name}}"
     allowed_domains = ["xxx.com"]
     start_urls = [
-        "http://baike.baidu.com/renwu"
+        ""
     ]
+
+##    post方法
+##    def start_requests(self):
+##        payload = None
+##        return [scrapy.http.FormRequest(url=start_url,formdata=payload,
+##                    callback=self.parse, dont_filter=True) for start_url in self.start_urls]
 
     def parse(self, response):
         pass
 
     def test(self, response):
+        '''测试方法，回调进入交互式测试'''
         from scrapy.shell import inspect_response
         inspect_response(response, self)
